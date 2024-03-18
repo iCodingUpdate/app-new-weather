a1aaef16070efd41fa334a68dbf573a3e0573927
Tati <153294267+iCodingUpdate@users.noreply.github.com>
2024-03-18 03:02:13 +0300

    Updates

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
